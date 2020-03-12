4503c63b69de4ca273f58cd83d91e253d67f5af4
Daniel Caraway <danielcaraway42@gmail.com>
2020-03-12 16:09:10 -0700

    Adding dash

--- app.py
+++ app.py
@@ -1,12 +1,39 @@
-from flask import Flask
 from flask import Flask, render_template, url_for
+# import dash
 
-app = Flask(__name__)
+# # app = dash.Dash(__name__)
+# app = Flask(__name__)
 
-@app.route('/')
-def index():
-    # return "Hello, world!"
-    return render_template('index.html')
+# @app.route('/')
+# def index():
+#     # return "Hello, world!"
+#     return render_template('index.html')
+import os
 
-if __name__ == "__main__":
-    app.run(debug=True)
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+
+external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+
+app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+
+server = app.server
+
+app.layout = html.Div([
+    html.H2('PICK A CITY, ANY CITY!'),
+    dcc.Dropdown(
+        id='dropdown',
+        options=[{'label': i, 'value': i} for i in ['LA', 'NYC', 'MTL']],
+        value='LA'
+    ),
+    html.Div(id='display-value')
+])
+
+@app.callback(dash.dependencies.Output('display-value', 'children'),
+              [dash.dependencies.Input('dropdown', 'value')])
+def display_value(value):
+    return 'You have selected "{}"'.format(value)
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
